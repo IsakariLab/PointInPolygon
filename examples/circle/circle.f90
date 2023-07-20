98d2d6a3cd5674bc971c97d432fbcf131a87c717
isakari <isakari@nuem.nagoya-u.ac.jp>
2023-07-20 09:54:20 +0900

    modifiled circle

--- examples/circle/circle.f90
+++ examples/circle/circle.f90
@@ -1,47 +1,52 @@
 program main
   implicit none
-  integer,parameter :: n=1000
+
+  integer,parameter :: n=500
+  real(8),parameter :: cx=0.d0, cy=0.d0, rad=1.d0
   real(8),parameter :: pi=acos(-1.d0)
-  real(8),parameter :: rad=0.5d0
-  integer :: i, j, itmp
-  real(8) :: cx, cy, th, xx, yy
+  character(len=256) :: filename="circle_c00_r1_n500"
+
+  integer :: i, itmp
+  real(8) :: th, xx, yy
   integer :: np, ne
   integer :: nd(2,n)
   real(8) :: x(2,n)
 
-  open(1,file="circle.el2")
+  
+  
+  open(1,file=trim(trim(filename)//".el2"))
   write(1,*) n
   do i=1,n
      th=2.d0*pi*dble(i-1)/dble(n)
-     xx=rad*cos(th)
-     yy=rad*sin(th)
+     xx=cx+rad*cos(th)
+     yy=cy+rad*sin(th)
      write(1,*) i, xx, yy
   end do
   write(1,*) n
   do i=1,n-1
      write(1,*) i, i+1, i, 1, 2
   end do
-  write(1,*) n, 1, i, 1, 2
+  write(1,*) n, 1, n, 1, 2
   close(1)
 
-  open(1,file="circle.el2")
-    read(1,*) np
-    do i=1,np
-      read(1,*) itmp, x(:,i)
-    end do
-    read(1,*) ne
-    do i=1,ne
-      read(1,*) itmp, nd(:,i)
-    end do
+  open(1,file=trim(trim(filename)//".el2"))
+  read(1,*) np
+  do i=1,np
+     read(1,*) itmp, x(:,i)
+  end do
+  read(1,*) ne
+  do i=1,ne
+     read(1,*) itmp, nd(:,i)
+  end do
   close(1)
 
-  open(1,file="circle.gp")
-    do i=1,ne
-      write(1,*) x(:,nd(1,i))
-      write(1,*) x(:,nd(2,i))
-      write(1,*)
-      write(1,*)
-    end do
-    close(1)
+  open(1,file=trim(trim(filename)//".gp"))
+  do i=1,ne
+     write(1,*) x(:,nd(1,i))
+     write(1,*) x(:,nd(2,i))
+     write(1,*)
+     write(1,*)
+  end do
+  close(1)
 
 end program main
